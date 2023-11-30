f977056e524c210aea9747a96f984fe1170d2a47
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-11-30 20:37:00 +0200

    update

--- 002/array.js
+++ 002/array.js
@@ -19,16 +19,14 @@ colors1.forEach((color, index) => {if (color == 'pink'){colors1[index] = 'black'
 });
 console.log(colors1);
 
-colors2.forEach((color, index) => {
-    if (color == 'pink'){
-        colors2[index] = 'skyblue';
+colors2.forEach((color, index) => {if (color == 'pink'){colors2[index] = 'skyblue';
     }
 });
 console.log(colors2);
 
 // mapas masyvo modifikacijai atlikti
-
-console.log('----------------')
+// pinks to black
+console.log('2. ----------------')
 const colors22 = [
     { name: 'pink' },
     { name: 'orange' },
@@ -38,16 +36,14 @@ const colors22 = [
     { name: 'indigo' },
     { name: 'pink' }
 ];
+
+const colors33 = colors22.map(color => color.name == 'pink' ? {name: 'black'} : color);
+console.log(colors33);
+
+
+
 // pinks to black
-
-
-console.log('--------------');
-const colors222 = colors22.map(color => color.name == 'pink' ? {name: 'black'} : color);
-console.log(colors222);
-
-
-
-console.log('--------------');
+console.log('3. --------------');
 const colors44 = [
     {name: 'pink', age: 12},
     {name: 'orange', age: 13},
@@ -61,10 +57,9 @@ const colors44 = [
 const colors444 = colors44.map(color => color.name == 'pink' ? {...color, name: 'black'} : color);
 console.log(colors444);
 
+
 //visas spalvas pakeisti i black
-
-
-console.log('--------------');
+console.log('4--------------');
 const colors441 = [
     {name: 'pink', age: 12},
     {name: 'orange', age: 13},
@@ -78,14 +73,14 @@ const colors441 = [
 const colors442 = colors441.map(color => ({...color, name: 'black'}));
 console.log(colors442);
 
-console.log('--------------');
-//masyvas bepukiu
+//masyvas be pukiu
+console.log('5.--------------');
 
 const cats = [
     {name: 'Tomas', age: 12},
     {name: 'Pukis', age: 13},
     {name: 'Juodis', age: 14},
-    {name: 'Margis', age: 15, tractor: 'John Deere'},
+    {name: 'Margis', age: 15},
     {name: 'Ryzas', age: 16},
     {name: 'Pukis', age: 17},
     {name: 'Pukis', age: 18}
@@ -95,18 +90,30 @@ const catsFiltered = cats.filter(cat => cat.name !== 'Pukis');
 console.log(catsFiltered);
 
 //be Juodzio ir +1 metai
-console.log('--------------');
+console.log('6.--------------');
 const cats2 = [
     {name: 'Tomas', age: 12},
     {name: 'Juodis', age: 13},
     {name: 'Juodis', age: 14},
-    {name: 'Margis', age: 15, tractor: 'John Deere'},
+    {name: 'Margis', age: 15},
     {name: 'Ryzas', age: 16},
     {name: 'Pukis', age: 17},
     {name: 'Juodis', age: 18}
 ];
 
-let noJuodisPlusOne = cats2.filter(cat => cat.name !== 'Juodis').map(cat => ({...cat, age: cat.age + 1}));
+let noJuodisPlusOne = cats2.filter(cat => cat.name !== 'Juodis')
+.map(cat => ({...cat, age: cat.age + 1}));
 console.log(noJuodisPlusOne);
 
-cats2.find((cat, index) =>{console.log(index)})
+//find(...)
+console.log('7.--------------');
+let counter = 0;
+const what = 1;
+const j = cats2.find(cat => {
+    if (cat.name == 'Margis') {
+        counter++;
+    }
+    return counter == what;
+});
+console.log(j);
+// cats2.find((cat, index) =>{console.log(index)})
