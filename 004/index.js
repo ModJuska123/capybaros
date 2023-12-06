550476477a7fe5ebc218cd9b9db5ddb951ba0ae2
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-12-06 09:52:23 +0200

    update

--- 004/index.js
+++ 004/index.js
@@ -26,7 +26,7 @@ class Stikline {
 
         return this;
     }
-
+// } nespejau..................
 
 
 }
--- 004/sm.js
+++ 004/sm.js
@@ -1,3 +1,6 @@
+//Nauja tema: Set(as), turi specifines savybes
+
+
 const array = [];
 
 array.push(10);
@@ -5,9 +8,41 @@ array.push(10);
 const set = new Set();
 
 set.add(10);
-set.add(5);
 set.add(7);
+set.add(7);
+set.add(7);
+set.add(10);
+set.add(1235);
+set.add(71);
 set.add(6);
 set.delete(7);
+console.log(set.has(10), set.size);
+
+const setArray = [...set];
+setArray.sort((a, b) => a - b);
+console.log(setArray);
+
+set.clear();
+setArray.forEach(item=>set.add(item));
+
 console.log(set);
 
+randSet = new Set();
+
+while(rand.size < 10) {
+    rand.Set.add(round(0, 11))
+};
+
+console.log(randSet);
+
+//MAPas, nauja tema
+
+const map = new Map();
+map.set('petras', 39);
+map.set('petras', 39);
+map.set('petras', 39);
+map.set('petras', 39);
+
+for(const item of map) {
+    console.log(key, value);
+}
