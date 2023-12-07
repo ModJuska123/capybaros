65d47d9a49e6a0f626fe07d8682ca41472e90969
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-12-07 14:28:56 +0200

    update

---
+++ r1/src/Components/BooksContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+//createContext() specialus komponentas
+export const BooksContext = createContext(); 
+
+    const booksUrl = 'https://in3.dev/knygos/';
+    // const typesUrl = 'https://in3.dev/knygos/types/';
+
+//naujas komponentas
+export const BooksProvider = ({children}) => {
+
+    const [books, setBooks] = useState(null); // use Steitas
+
+    useEffect(_ => {       // use Efectas
+        axios.get(booksUrl)
+        .then(res => {setBooks(res.data);
+            console.log(res.data);
+        });
+    }, []);
+
+
+
+  return (<BooksContext.Provider value={{books}}>
+      {children}
+    </BooksContext.Provider>
+  )
+}
