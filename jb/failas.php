2baac66d4bf780fdd3e7cffb8324f333d6b12e09
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-12-12 13:12:44 +0200

    update

---
+++ jb/failas.php
@@ -0,0 +1,4 @@
+<h1>Big</h1>
+
+<?php
+echo 'Labas rytas'
---
+++ jb/r.js
@@ -0,0 +1,6 @@
+const root = document.getElementById('root');
+
+
+const h1 = document.createElement('h1');
+h1.textContent = 'Labas rytas';
+root.appendChild(h1);
